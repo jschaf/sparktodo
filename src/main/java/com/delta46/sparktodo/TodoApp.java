1dafed987fe37ae1efa63e0f3fd9c8c3c55592cb
Joe Schafer <joe@jschaf.com>
2016-01-25 00:49:06 -0500

    fix: pick port number based on $PORT

--- src/main/java/com/delta46/sparktodo/TodoApp.java
+++ src/main/java/com/delta46/sparktodo/TodoApp.java
@@ -4,7 +4,22 @@ import static spark.Spark.*;
 
 public class TodoApp {
 
+    public static final int DEFAULT_PORT = 4567;
+
+    private static int getPortNumber() {
+        String portString = System.getenv("PORT");
+        if (portString == null) {
+            return DEFAULT_PORT;
+        } else {
+            int port = Integer.parseInt(portString);
+            return port;
+        }
+    }
+
     public static void main(String[] args) {
+
+        port(getPortNumber());
+
         get("/hello", (req, res) -> "Hello World");
 
         get("/hello/:name", (request, response) ->
