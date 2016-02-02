2af30e4aab3628c92ba378db2d55ff68170de723
Joe Schafer <joe@jschaf.com>
2016-02-02 00:24:10 -0500

    fix: make Jackson, Lombok and immutable POJOs play nice

--- src/main/java/todoer/api/TodoEntry.java
+++ src/main/java/todoer/api/TodoEntry.java
@@ -1,11 +1,14 @@
 package todoer.api;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.Wither;
 
-@Value
+@JsonDeserialize(builder = TodoEntry.TodoEntryBuilder.class)
 @Builder
+@Value
 public class TodoEntry {
     @Wither
     private final Integer id;
@@ -18,4 +21,9 @@ public class TodoEntry {
 
     @Wither
     private final int ordering;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static final class TodoEntryBuilder {
+
+    }
 }
